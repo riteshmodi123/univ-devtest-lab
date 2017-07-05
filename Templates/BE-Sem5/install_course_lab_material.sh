f7c4d7aa98000de5bc19b56553d30867665013c2
Maninderjit Bindra <mani@Saurabhs-MacBook-Pro.local>
2017-07-05 14:23:14 +0530

    modified script name

--- Templates/BE-Sem5/install_course_lab_material.sh
+++ Templates/BE-Sem5/install_course_lab_material.sh
@@ -13,7 +13,7 @@ apt-get -y install apache2
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
 # restart Apache
-apachectl restartSaurabhs-MacBook-Pro:Templates mani$
+apachectl restart
 
 
 # TO ADD
